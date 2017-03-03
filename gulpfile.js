a7649b916007074351759defefd715d1d5568032
shawn sandy <shawnsandy04@gmail.com>
2017-03-03 16:23:20 -0500

    refactor - removed the sass error directive

--- gulpfile.js
+++ gulpfile.js
@@ -13,23 +13,22 @@ var notify = require('gulp-notify');
 var changed = require('gulp-changed');
 var toast = require('node-notifier');
 
-gulp.task('copy:js', function () {
+gulp.task('copy:js', function() {
     gulp.src(['/node'])
         .pipe(gulp.dest('./src/public/assets/'));
 });
 
-gulp.task('build', function () {
+gulp.task('build', function() {
     gulp.src([
-        './node_modules/font-awesome/css/**/*.*',
-        './node_modules/font-awesome/fonts/**/*.*'
-    ], {'base': 'node_modules'})
+            './node_modules/font-awesome/css/**/*.*',
+            './node_modules/font-awesome/fonts/**/*.*'
+        ], { 'base': 'node_modules' })
         .pipe(gulp.dest('./src/public/assets/'))
 });
 
-gulp.task('sass', function () {
-    return gulp.src('./_sass/**/*.scss', {'base': './_sass/'})
-        .pipe(sass().on('error', notify.onError('Error processing')))
-        .pipe(sass().on('error', sass.logError))
+gulp.task('sass', function() {
+    return gulp.src('./_sass/**/*.scss', { 'base': './_sass/' })
+        .pipe(sass().on('error', notify.onError("Error: <%= error.message %>")))
         .pipe(gulp.dest('builds/'))
         .pipe(notify({
             title: "Pages Notification",
@@ -39,6 +38,6 @@ gulp.task('sass', function () {
 
 });
 
-gulp.task('watch:sass', function () {
+gulp.task('watch:sass', function() {
     gulp.watch('./_sass/css/**/*.scss', ['sass']);
 });
